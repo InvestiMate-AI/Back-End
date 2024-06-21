2412c20a1bd6cb7c57127ad3a31c0e65d10030ef
JuseungL <121665437+JuseungL@users.noreply.github.com>
2024-06-21 15:40:13 +0900

    fix again

--- src/main/java/com/server/InvestiMate/api/TradeHistory/dto/request/TradeHistorySaveRequestDto.java
+++ src/main/java/com/server/InvestiMate/api/TradeHistory/dto/request/TradeHistorySaveRequestDto.java
@@ -2,11 +2,12 @@ package com.server.InvestiMate.api.TradeHistory.dto.request;
 
 import com.server.InvestiMate.api.TradeHistory.domain.TradeType;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 public record TradeHistorySaveRequestDto (
         @NotBlank String stockCode,
-        @NotBlank TradeType tradeType,
+        @NotNull TradeType tradeType,
         @Positive int tradeAmount
 ){
 }
