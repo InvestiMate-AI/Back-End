c096c3afb9d04331a64e8f2b1b4162d12cb000dd
JuseungL <121665437+JuseungL@users.noreply.github.com>
2024-06-21 15:36:29 +0900

    fix validator - close #29

--- src/main/java/com/server/InvestiMate/api/TradeHistory/dto/request/TradeHistorySaveRequestDto.java
+++ src/main/java/com/server/InvestiMate/api/TradeHistory/dto/request/TradeHistorySaveRequestDto.java
@@ -2,10 +2,11 @@ package com.server.InvestiMate.api.TradeHistory.dto.request;
 
 import com.server.InvestiMate.api.TradeHistory.domain.TradeType;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 
 public record TradeHistorySaveRequestDto (
         @NotBlank String stockCode,
         @NotBlank TradeType tradeType,
-        @NotBlank int tradeAmount
+        @Positive int tradeAmount
 ){
 }
